b61ef687704ccd58ca15a52dce8717711ff91249
zaru <zarutofu@gmail.com>
2020-05-12 22:00:38 +0900

    fix electron window config

--- main.js
+++ main.js
@@ -5,21 +5,23 @@ const {app, BrowserWindow} = require('electron');
  */
 function createWindow() {
   const win = new BrowserWindow({
-    width: 800,
-    height: 1200,
+    width: 640,
+    height: 480,
     hasShadow: false,
     transparent: true,
     frame: false,
     resizable: false,
-    alwaysOnTop: true,
-    fullscreen: false,
   });
+  // MagickCode
   app.dock.hide();
+  win.setAlwaysOnTop(true, 'floating');
   win.setVisibleOnAllWorkspaces(true);
+  win.setFullScreenable(false);
+  app.dock.show();
 
-  win.loadFile('pages/index.html');
+  win.loadFile('./dist-bodypix-app/index.html');
 
-  // win.webContents.openDevTools()
+  // win.webContents.openDevTools();
 }
 
 app.whenReady().then(createWindow);
